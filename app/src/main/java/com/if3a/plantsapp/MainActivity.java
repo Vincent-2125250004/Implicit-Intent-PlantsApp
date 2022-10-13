77d1658bc045c9d3d340a03bbfdb885a364f887e
Vincent-2125250004 <vincent.cent@mhs.mdp.ac.id>
2022-10-13 20:15:20 +0700

    PlantsApp

---
+++ app/src/main/java/com/if3a/plantsapp/MainActivity.java
@@ -0,0 +1,41 @@
+package com.if3a.plantsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn_login, btn_signup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_login= findViewById(R.id.btn_login);
+        btn_signup= findViewById(R.id.btn_signup);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this,SignUpActivity.class);
+                intent.setData(Uri.parse("https://plnts.com"));
+                startActivity(intent);
+            }
+        });
+    }
+}
